c65ff3300b980aabcd51dd00bc7d93e1631bfdca
IgoorToorres <igorevangelista121@gmail.com>
2026-03-18 10:30:39 -0300

    fix: removendo o campo name de users

--- db/migrations/20260318130554_create_users.ts
+++ db/migrations/20260318130554_create_users.ts
@@ -3,7 +3,6 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
-    table.text('name').notNullable()
   })
 }
 
