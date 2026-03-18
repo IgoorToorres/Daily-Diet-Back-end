4ff0fb04b0647d5d61c6b938aa93e3088303e9c9
IgoorToorres <igorevangelista121@gmail.com>
2026-03-18 10:52:17 -0300

    feat: checkando se usuario ja esta "logado" via cookie

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,28 @@
 import type { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { db } from '../database'
+import { checkUserIdExist } from '../middlewares/check-user-id-exist'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
+  app.get('/', { preHandler: [checkUserIdExist] }, async () => {
     return console.log('cheguei aqui')
+  })
+
+  app.post('/', async (request, reply) => {
+    let userId = request.cookies.userId
+    if (!userId) {
+      userId = randomUUID()
+      await db('users').insert({
+        id: randomUUID(),
+      })
+      reply.cookie('userId', userId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7days
+      })
+    }
+
+    return reply.status(201).send({
+      message: 'usuario cadastrado com sucesso',
+    })
   })
 }
