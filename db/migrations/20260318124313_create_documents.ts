eeddca5febe62e264b1dd2036a34cc8afe73ba45
IgoorToorres <igorevangelista121@gmail.com>
2026-03-18 10:08:59 -0300

    config: migration de users

--- db/migrations/20260318124313_create_documents.ts
+++ db/migrations/20260318124313_create_documents.ts
@@ -12,10 +12,16 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
+    table
+      .uuid('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
     table.text('name').notNullable()
     table.text('description').notNullable()
     table.timestamp('consumed_at').notNullable()
-    table.boolean('isOnDiet').notNullable()
+    table.boolean('is_on_diet').notNullable()
   })
 }
 
