4760115c48b96b2bdbcf53960a9221d24d67a767
IgoorToorres <igorevangelista121@gmail.com>
2026-03-18 10:02:41 -0300

    config: migration meals criada

--- db/migrations/20260318124313_create_documents.ts
+++ db/migrations/20260318124313_create_documents.ts
@@ -1,5 +1,24 @@
 import type { Knex } from 'knex'
 
-export async function up(knex: Knex): Promise<void> {}
+// export const Meal = {
+//   id: "8",
+//   name: "Hambúrguer artesanal",
+//   description: "sanduiche de pao integral com atum e salada de alface e tomate",
+//   date: "11/08/2003",
+//   time: "21:00",
+//   isOnDiet: false,
+// }
 
-export async function down(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.timestamp('consumed_at').notNullable()
+    table.boolean('isOnDiet').notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
